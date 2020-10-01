9a096fa9587a525680e4dc25df8e2026e6c495ae
David Temple <dstemple7@gmail.com>
2020-10-01 15:58:45 -0500

    markov & histo working

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -6,8 +6,33 @@ with open("input.txt") as f:
 
 # TODO: analyze which words can follow other words
 # Your code here
+wordsArray = words.split()
+wordsStart = []
+wordsNext = {}
 
+for i,word in enumerate(wordsArray):
+    if not word.islower():
+        wordsStart.append(word)
+    if word in wordsNext:
+        wordsNext[word].append(wordsArray[i + 1])
+    else:
+        if i + 1 < len(wordsArray):
+            wordsNext[word] = [wordsArray[i + 1]]
 
 # TODO: construct 5 random sentences
 # Your code here
+punct = {".", "?", "!"}
+
+def randSentence():
+    output = word = random.choice(wordsStart)
+    while len(word) < 1 or (not word[-1] in punct and not word[-2] in punct):
+        word = random.choice(wordsNext[word])
+        output = output + " " + word
+    return output
+
+print(randSentence())
+print(randSentence())
+print(randSentence())
+print(randSentence())
+print(randSentence())
 
